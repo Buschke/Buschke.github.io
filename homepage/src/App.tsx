66fe691d0ac536a4419cc1aab6f987992bf85a89
Buschke <github.com@buschke.com>
2022-05-09 16:23:57 +0200

    3 files modified, 3 added, 3 deleted

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -9,7 +9,7 @@ interface AppProps {
 export const App: FC<AppProps> = ({ title }) => {
   return (
     <div className='App'><h1>{title}</h1>
-    to the homepage of Sven Buschke, status: 
+    to the homepage of Sven Buschke, status: online
     </div>
   )
 }
