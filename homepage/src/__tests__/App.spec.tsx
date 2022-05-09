b0e73a37c3cbd842afc92fa4579af0114d3764d9
Buschke <github.com@buschke.com>
2022-05-09 22:31:23 +0200

    added testing

---
+++ homepage/src/__tests__/App.spec.tsx
@@ -0,0 +1,42 @@
+import { assert, expect, test } from 'vitest'
+import { describe, it } from 'vitest'
+import { App } from '../App'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+
+describe('App', () => {
+  it('Title is "App"', async () => {
+    render(<App title='Sven' />)
+    const button =  screen.getByTestId('button')
+    expect(button.textContent).toBe('Test')
+    userEvent.click(button)
+    await waitFor(() => expect(button.textContent).toBe('Tested'))
+  })
+})
+
+// Edit an assertion and save to see HMR in action
+
+test('Math.sqrt()', () => {
+  expect(Math.sqrt(4)).toBe(2)
+  expect(Math.sqrt(144)).toBe(12)
+  expect(Math.sqrt(2)).toBe(Math.SQRT2)
+})
+
+test('JSON', () => {
+  const input = {
+    foo: 'hello',
+    bar: 'world',
+  }
+
+  const output = JSON.stringify(input)
+
+  expect(output).eq('{"foo":"hello","bar":"world"}')
+  assert.deepEqual(JSON.parse(output), input, 'matches original')
+})
+
+describe('Simple App test', () => {
+  it('Run: is 3 3', () => {
+    expect(3).toBe(3)
+  })
+})
